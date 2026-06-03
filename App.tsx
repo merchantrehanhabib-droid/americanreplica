1aa24dd3c4840a82bb8b2aa188c80af843d2fe73
merchantrehanhabib-droid <merchantrehanhabib@gmail.com>
2026-06-03 19:22:20 +0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { Switch, Route, Router as WouterRouter } from "wouter";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
+import { Toaster } from "./toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/Home";
